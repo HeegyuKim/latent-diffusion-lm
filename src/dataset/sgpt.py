753a06169dbe3f3f9e1c8197b86b823420910ee4
HeegyuKim <heekue83@gmail.com>
2022-10-19 01:10:02 +0000

    sgpt-mini 도 해봄

--- src/dataset/sgpt.py
+++ src/dataset/sgpt.py
@@ -15,28 +15,10 @@ def normalize_weights(weights):
     s = sum(weights)
     return [w / s for w in weights]
 
-def pad_sentence_latents(sents: torch.Tensor, max_seq_len: int):
-    """
-        sents: [s, dim]
-
-        out: [max_s, dim]
-    """
-    pad_size = max_seq_len - sents.shape[0]
-    if pad_size == 0:
-        return sents
-    elif pad_size < 0:
-        return sents[:max_seq_len]
-    else:
-        pad = torch.zeros(
-            (pad_size, sents.shape[1]),
-            device = sents.device,
-            dtype=sents.dtype
-        )
-        return torch.cat([sents, pad], dim=0)
 
 @dataclass
 class SGPTDataset(Dataset):
-    encoder: Any
+    # encoder: Any
     dataset_paths: List[str]
     max_seq_len: int
     column: str = "sentences"
@@ -63,17 +45,8 @@ class SGPTDataset(Dataset):
         dataset = self._get_dataset()
 
         item = dataset[index]
-        sents = self.encoder.encode(item[self.column])
-        mask_len = min(len(sents), self.max_seq_len)
-        attention_mask = [1] * mask_len + [0] * (self.max_seq_len - mask_len)
+        return item
 
-        sents = pad_sentence_latents(sents, self.max_seq_len + 1)
-        
-        return {
-            "inputs": sents[:-1],
-            "attention_mask": torch.LongTensor(attention_mask),
-            "labels": sents[1:]
-        }
 
 @dataclass
 class SGPTIterableDataset(IterableDataset):
--- src/task/sgpt.py
+++ src/task/sgpt.py
@@ -18,6 +18,7 @@ from hydra.utils import get_original_cwd, to_absolute_path
 from ..dataset.sgpt import SGPTDataset
 from .optimus_v2 import OptimusTask
 from tqdm import tqdm
+from collections import defaultdict
 import wandb
 
 
@@ -30,6 +31,26 @@ def freeze_model(model):
     for p in model.parameters():
         p.requires_grad = False
 
+def pad_sentence_latents(sents: torch.Tensor, max_seq_len: int):
+    """
+        sents: [s, dim]
+
+        out: [max_s, dim]
+    """
+    pad_size = max_seq_len - sents.shape[0]
+    if pad_size == 0:
+        return sents
+    elif pad_size < 0:
+        return sents[:max_seq_len]
+    else:
+        pad = torch.zeros(
+            (pad_size, sents.shape[1]),
+            device = sents.device,
+            dtype=sents.dtype
+        )
+        return torch.cat([sents, pad], dim=0)
+
+
 class SGPTTask(BaseTask):
 
     def __init__(self, config: DictConfig) -> None:
@@ -40,9 +61,15 @@ class SGPTTask(BaseTask):
         self.model = GPT2Model(gpt2_config)
 
     
+    def get_train_collator(self) -> Callable:
+        return lambda x: x
+
+    def get_eval_collator(self) -> Callable:
+        return lambda x: x
+
     def get_train_dataset(self) -> Dataset:
         return SGPTDataset(
-            self.autoencoder,
+            # self.autoencoder,
             self.config.dataset.train,
             self.config.model.max_seq_len,
             column="utterances",
@@ -51,14 +78,37 @@ class SGPTTask(BaseTask):
 
     def get_eval_dataset(self) -> Dataset:
         return SGPTDataset(
-            self.autoencoder,
+            # self.autoencoder,
             self.config.dataset.test,
             self.config.model.max_seq_len,
             column="utterances",
             split="test"
         )
+
+    def _item_for_train(self, sents):
+        sents = self.autoencoder.encode(sents)
+        mask_len = min(len(sents), self.config.model.max_seq_len)
+        attention_mask = [1] * mask_len + [0] * (self.config.model.max_seq_len - mask_len)
+
+        sents = pad_sentence_latents(sents, self.config.model.max_seq_len + 1)
         
+        return {
+            "inputs": sents[:-1],
+            "attention_mask": torch.LongTensor(attention_mask),
+            "labels": sents[1:]
+        }
+
     def step(self, batch, batch_idx) -> dict:
+        new_batch = defaultdict(list)
+        for b in batch:
+            item = self._item_for_train(b["utterances"])
+            for k, v in item.items():
+                new_batch[k].append(v)
+        
+        batch = {}
+        for k, v in new_batch.items():
+            batch[k] = torch.stack(v).to(self.device)
+
         mask = batch["attention_mask"]
         hidden = self.model(
             inputs_embeds=batch["inputs"],
@@ -82,7 +132,7 @@ class SGPTTask(BaseTask):
         loss, outputs = self.step(batch, batch_idx)
 
         out = {"loss": loss}
-        self.log_dict(out, prefix="val_", on_epoch=True)
+        self.log_dict(out, prefix="val_", on_epoch=True, batch_size=len(batch))
 
         # if batch_idx < 2:
         #     print(batch["sentences"])
